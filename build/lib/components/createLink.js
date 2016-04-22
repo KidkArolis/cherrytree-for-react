da8d8bb3446bdf83b6540b4ac4842e12a39fceb0
Oliver Woodings <oliver.woodings@gmail.com>
2016-04-22 09:18:04 +0100

    Use spread operator instead of assign

--- build/lib/components/createLink.js
+++ build/lib/components/createLink.js
@@ -3,15 +3,12 @@
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 exports['default'] = createLink;
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
 var _react = require('react');
-
-var _reactLibObjectAssign = require('react/lib/Object.assign');
-
-var _reactLibObjectAssign2 = _interopRequireDefault(_reactLibObjectAssign);
 
 var object = _react.PropTypes.object;
 var string = _react.PropTypes.string;
@@ -54,7 +51,7 @@ function createLink() {
     render: function render() {
       var router = this.context.router;
 
-      var props = (0, _reactLibObjectAssign2['default'])({}, this.props);
+      var props = _extends({}, this.props);
 
       // Ignore if rendered outside of the context of a
       // router, simplifies unit testing.
